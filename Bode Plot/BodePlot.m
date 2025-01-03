3c30c733543808e3e874a5e9f17fc5e0a95bcc4e
Omar Fayed <omarfayed381@gmail.com>
2025-01-03 23:33:03 +0200

    another Bode plot example

--- Bode Plot/BodePlot.m
+++ Bode Plot/BodePlot.m
@@ -4,3 +4,7 @@ sys = 500 / (s * (s + 10) * (s + 20));
 bodeplot(sys);
 grid on
 %% section 2
+s = tf('s');
+sys = (1 * (1 + 0.2 * s)) / ((0.1 * s + 1) * (2 + 3 * s + s * s));
+bodeplot(sys);
+grid on
